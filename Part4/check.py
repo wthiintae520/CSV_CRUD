1ce9afba711b7bd7a30fd22d43dd18ff7331fa7d
Yunting Yin <wthiintae520@gmail.com>
2022-12-04 02:22:23 -0500

    removed check.py and test.csv

--- Part4/check.py
+++
@@ -1,3 +0,0 @@
-with open("C:/Desk/CP/Level 4/CST8333 Programming Language Research Project/git/CSV_CRUD/Part4/NAFO-4TVN-Atlantic-Cod-otoliths.csv", 'r') as f_in:
-    for line in f_in:
-        print(line)
